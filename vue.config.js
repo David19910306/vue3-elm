da2865f78288f8bd3ca6923ac98e7a7a4ef69886
David19910306 <494626043@qq.com>
2022-03-14 02:02:54 +0800

    同步

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 function resolve (dir) {
   return path.join(__dirname, dir)
 }
-
+// 提交代码到gitee，并同步到github
 module.exports = {
   publicPath: '/',
   chainWebpack: config => {
