4265ae0bbb2f2cfe18356c163a84230d943fbf19
David19910306 <494626043@qq.com>
2022-04-18 19:24:10 +0800

    订单和我的界面搭建完成

--- src/views/Main/List/index.vue
+++ src/views/Main/List/index.vue
@@ -1,14 +1,46 @@
 <template>
   <div class="list">
-    List
+    <Header>
+      <template v-slot:left><span><i class="iconfont icon-fangxiang-zuo arrowLeft leftIcon"></i></span></template>
+      <template v-slot:center><span class="detail-center">订单列表</span></template>
+    </Header>
+    <section class="list-container">
+      <ul class="list-ul">
+        <li>
+          <div class="shop-avatar">
+            <img src="https://elm.cangdu.org/img/17c4ff59a35100186.png" />
+          </div>
+          <div class="list-detail">
+            <header class="shop-name">
+              <section class="order-name">
+                <h4>古天杀杀杀1111111111<i class="iconfont icon-fangxiang-you"></i></h4>
+                <p class="order-time">2022-04-02 20:03</p>
+              </section>
+              <section class="delay">支付超时</section>
+            </header>
+            <section class="order-list-detail">
+              <p>水电费 等2件商品</p>
+              <span class="total-price">¥3079.00</span>
+            </section>
+            <footer class="order-again">
+              <Button plain size="mini" color="#3190e8"
+                :style="{'--van-button-border-radius': '3px'}">再来一单</Button>
+            </footer>
+          </div>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { Button } from 'vant'
+import Header from '@/components/header/index.vue'
 
 export default defineComponent({
   name: 'MainList',
+  components: { Header, Button },
   setup () {
     console.log('@')
   }
@@ -16,5 +48,93 @@ export default defineComponent({
 </script>
 
 <style lang="less" scoped>
-
+.list{
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .detail-center{
+    font-size: .2rem;
+    font-weight: 700;
+  }
+  .arrowLeft{
+    font-size: .25rem;
+  }
+  .list-container{
+    overflow-y: auto;
+    .list-ul{
+      li:last-child{
+        margin-bottom: 0;
+      }
+      li{
+        display: flex;
+        background-color: #fff;
+        padding: .1rem;
+        margin-bottom: .1rem;
+        .shop-avatar{
+          width: .47rem;
+          height: .47rem;
+          margin-right: .05rem;
+          img{
+            width: 100%;
+            height: 100%;
+          }
+        }
+        .list-detail{
+          flex: 1;
+          .shop-name{
+            display: flex;
+            justify-content: space-between;
+            align-items: baseline;
+            padding-bottom: .05rem;
+            border-bottom: .006rem solid #f5f5f5;
+            .order-name{
+              h4{
+                font-size: .17rem;
+                color: #333;
+                line-height: .23rem;
+                width: 2.1rem;
+              }
+              .order-time{
+                font-size: .13rem;
+                color: #999;
+                line-height: .18rem;
+              }
+            }
+            .delay{
+              color: #333;
+              font-size: .14rem;
+            }
+          }
+          .order-list-detail{
+            display: flex;
+            justify-content: space-between;
+            line-height: .35rem;
+            align-items: center;
+            border-bottom: .006rem solid #f5f5f5;
+            p{
+              font-size: .14rem;
+              color: #666;
+              width: 2rem;
+              overflow: hidden;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+            }
+            .total-price{
+              font-size: .14rem;
+              color: #f60;
+              font-weight: 700;
+              font-family: Helvetica Neue,Tahoma,Arial;
+            }
+          }
+          .order-again{
+            height: .4rem;
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
--- src/views/Main/Mine/index.vue
+++ src/views/Main/Mine/index.vue
@@ -1,14 +1,101 @@
 <template>
   <div class="mine">
-    Mine
+    <Header>
+      <template v-slot:left><span><i class="iconfont icon-fangxiang-zuo arrowLeft leftIcon"></i></span></template>
+      <template v-slot:center><span class="detail-center">我的</span></template>
+    </Header>
+    <section class="profile-container">
+      <router-link to="">
+        <div class="register-login">
+          <img class="login-image" src="https://elm.cangdu.org/img/default.jpg" />
+            <div class="login">
+              <p>登录/注册</p>
+              <p>
+                <i class="iconfont icon-phone-iphone"></i>
+                <span>暂无绑定手机号</span>
+              </p>
+            </div>
+          <i class="iconfont icon-fangxiang-you arrowRight"></i>
+        </div>
+      </router-link>
+      <section class="info-data">
+        <div class="login-activity">
+          <span class="money-number"><b>0.00</b>元</span>
+          <span class="my-money">我的余额</span>
+        </div>
+        <div class="login-activity">
+          <span class="benefit-number"><b>0</b>个</span>
+          <span class="my-money">我的优惠</span>
+        </div>
+        <div class="login-activity">
+          <span class="score-number"><b>0</b>分</span>
+          <span class="my-money">我的积分</span>
+        </div>
+      </section>
+      <section class="profile-1reTe">
+        <ul class="profile-ul">
+          <router-link to="">
+            <li>
+              <i class="iconfont icon-dingdan1 icon-list" style="color: #aaa;"></i>
+              <span class="text-span">
+                <span>我的订单</span>
+                <i class="iconfont icon-fangxiang-you"></i>
+              </span>
+            </li>
+          </router-link>
+          <router-link to="">
+            <li>
+              <i class="iconfont icon-shangcheng icon-list" style="color: #FC7B53;"></i>
+              <span class="text-span">
+                <span>积分商城</span>
+                <i class="iconfont icon-fangxiang-you"></i>
+              </span>
+            </li>
+          </router-link>
+          <router-link to="">
+            <li>
+              <i class="iconfont icon-wodehuiyuan0101 icon-list" style="color: #FFC69A;"></i>
+              <span class="text-span">
+                <span>饿了么会员卡</span>
+                <i class="iconfont icon-fangxiang-you"></i>
+              </span>
+            </li>
+          </router-link>
+        </ul>
+      </section>
+      <section class="profile-1reTe">
+        <ul class="profile-ul">
+          <router-link to="">
+            <li>
+              <i class="iconfont icon-fuwuzhongxin icon-list" style="color: #3190e8;"></i>
+              <span class="text-span">
+                <span>服务中心</span>
+                <i class="iconfont icon-fangxiang-you"></i>
+              </span>
+            </li>
+          </router-link>
+          <router-link to="">
+            <li>
+              <i class="iconfont icon-elemo1 icon-list"></i>
+              <span class="text-span">
+                <span>下载饿了么APP</span>
+                <i class="iconfont icon-fangxiang-you"></i>
+              </span>
+            </li>
+          </router-link>
+        </ul>
+      </section>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Header from '@/components/header/index.vue'
 
 export default defineComponent({
   name: 'MainMine',
+  components: { Header },
   setup () {
     console.log('@')
   }
@@ -16,5 +103,140 @@ export default defineComponent({
 </script>
 
 <style lang="less" scoped>
+.mine{
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: #f5f5f5;
+  .detail-center{
+    font-size: .2rem;
+    font-weight: 700;
+  }
+  .arrowLeft{
+    font-size: .25rem;
+  }
+  .profile-container{
+    .register-login{
+      height: .9rem;
+      background-color: #3190e8;
+      display: flex;
+      padding: .06rem .1rem;
+      justify-content: space-between;
+      align-items: center;
+      .arrowRight{
+        color: #fff;
+        font-size: .2rem;
+      }
+      .login-image{
+        width: .58rem;
+        height: .58rem;
+        border-radius: 50%;
+        margin-right: .1rem;
+      }
+      .login{
+        flex: 1;
+        p{
+          font-size: .1875rem;
+          color: #fff;
+          font-weight: 700;
+          margin-bottom: .05rem;
+          i{
+            font-size: .15rem;
+            color: #fff;
+            font-weight: 400;
+          }
+          span{
+            font-size: .15rem;
+            color: #fff;
+            font-weight: 400;
+          }
+        }
+      }
 
+    }
+    .info-data{
+      width: 100%;
+      background-color: #fff;
+      box-sizing: border-box;
+      display: flex;
+      .login-activity{
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: .15rem 0;
+        border-right: 1px solid #f1f1f1;
+        .money-number{
+          font-size: .1289rem;
+          color: #333;
+          margin: 0.1rem;
+          b{
+            font-size: .2815rem;
+            color: #f90;
+            font-weight: 700;
+            line-height: .23rem;
+          }
+        }
+        .benefit-number{
+          font-size: .1289rem;
+          color: #333;
+          margin: 0.1rem;
+          b{
+            font-size: .2815rem;
+            color: #ff5f3e;
+            font-weight: 700;
+            line-height: .23rem;
+          }
+        }
+        .score-number{
+          font-size: .1289rem;
+          color: #333;
+          margin: 0.1rem;
+          b{
+            font-size: .2815rem;
+            color: #6ac20b;
+            font-weight: 700;
+            line-height: .23rem;
+          }
+        }
+        .my-money{
+          color: #666;
+          font-weight: 400;
+          font-size: .1343rem;
+        }
+      }
+    }
+    .profile-1reTe{
+      margin-top: .1rem;
+      background-color: #fff;
+      .profile-ul li{
+        padding: .13rem 0;
+        display: flex;
+        align-items: center;
+        border-bottom: 1px solid #f1f1f1;
+        .icon-list{
+          font-size: .18rem;
+          width: .25rem;
+          margin-left: .15rem;
+        }
+        .text-span{
+          flex: 1;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          span{
+            font-size: .164rem;
+            color: #333;
+            flex: 1;
+          }
+          i{
+            font-size: .18rem;
+            color: #aaa;
+            width: .25rem;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
